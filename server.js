24ad4699619464ab997e8fae299c4b274f5ddee4
sthefano <sthefanoss@gmail.com>
2022-07-10 15:49:06 -0300

    use server back

--- server.js
+++ server.js
@@ -1,9 +1,101 @@
-const http = require("http");
-const express = require("express");
+const express = require('express');
+//const cors = require('cors');
+const bodyParser = require("body-parser");
 const app = express();
+const port = 21147;
 
-app.use(function(req, res) {
-    res.send("<h1>Servidor rodando com ExpressJS</h1>");
+//app.use(cors({origin: '*'}));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  if(req.url == '/') {
+    res.redirect('http://app.carewhyapp.kinghost.net/');
+    return;
+  }
+  next();
 });
 
-http.createServer(app).listen(21147, () => console.log("Servidor rodando local na porta 21147"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+const users = [];
+
+const lups = [];
+
+app.get('/lups', (req, res) => {
+  res.json(lups.map( lup => { return {
+    author: users.find(user => user.id == lup.authorId),
+    id: lup.id,
+    title: lup.title,
+    description: lup.description,
+    collaborators: lup.collaboratorIds.map(id => users.find(user => user.id == id))    
+   };
+  }));
+})
+
+app.get('/users', (req, res) => {
+  let data = req.query; 
+  let token = data.token;
+  if(token == null) {
+    res.json(users);
+  }
+  res.json(users.filter(user => user.id != token));
+})
+
+app.get('/users/:id', (req, res) => {
+  let id = req.params.id;
+  let user = users.find(e => e.id == id);
+  if(user == undefined) {
+    res.status(400).send(`id not found ${users.length} ${id}`);
+  }
+  res.json(user);
+})
+
+app.post('/users', (req, res) => {
+  let user = req.query;
+  // apply validations
+  if(user.name == null) return res.status(400).send('name is required');
+  if(user.imageUri == null) return res.status(400).send('imageUri is required');
+  
+  let newUser = {
+    id: new Date().getTime(),
+    name: user.name,
+    imageUri: user.imageUri,
+  };
+
+  users.push(newUser);
+  res.json(newUser);
+});
+
+app.post('/lups', (req, res) => {
+  let data = req.query; 
+  let token = data.token;
+  if(data.title == null) return res.status(400).send('title is required');
+  if(data.description == null) return res.status(400).send('description is required');
+  
+  let newLup = {
+    authorId: token,
+    id: new Date().getTime(),
+    title: data.title,
+    description: data.description,
+    collaboratorIds: data.collaboratorIds || []
+  };
+  
+  lups.push(newLup);
+  res.json({
+    author: users.find(user => user.id == token),
+    id: newLup.id,
+    title: newLup.title,
+    description: newLup.description,
+    collaborators: newLup.collaboratorIds.map(id => { return users.find(user => user.id == id);})
+  });
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})								
