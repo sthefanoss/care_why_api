211ffff3ceaeb05090e4d320e1f7918045c29e22
sthefano <sthefanoss@gmail.com>
2022-07-10 14:43:50 -0300

    teste

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
-app.get("/", function(req, res) {
+app.use(function(req, res) {
     res.send("<h1>Servidor rodando com ExpressJS</h1>");
 });
 
