15f38f0149b48d8f155c90aa5164ead7fefec260
sthefano <sthefanoss@gmail.com>
2022-07-30 11:38:30 -0300

    remove trailing  's'

--- src/server.js
+++ src/server.js
@@ -51,7 +51,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   if(req.url == '/') {
     res.redirect('http://app.carewhyapp.kinghost.net/');
-    return;s
+    return;
   }
   next();
 });
