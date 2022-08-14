73959a774490da3a938cec7943ba5620ed69668c
sthefano <sthefanoss@gmail.com>
2022-08-13 22:28:42 -0300

    fix assets path

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/upload", express.static(__dirname + "/upload"));
+app.use("/upload", express.static("upload"));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
