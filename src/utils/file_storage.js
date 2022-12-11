9351fb06fc5a2276060c592da73d7103a07c0ee6
sthefano <sthefanoss@gmail.com>
2022-12-11 16:35:55 -0300

    make file for dirname

--- src/utils/file_storage.js
+++ src/utils/file_storage.js
@@ -1,8 +1,9 @@
 const multer = require("multer");
+const dirname = require("../../dirname");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, __dirname + '../../upload/')
+        cb(null, dirname + '/upload/')
     },
     filename: (req, file, cb) => {
         var fileFormat = (file.originalname).split(".");
