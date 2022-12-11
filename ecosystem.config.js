4ebdfc849772e1815735a6a8f5ccd51d30478872
sthefano <sthefanoss@gmail.com>
2022-12-11 17:37:07 -0300

    test

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   apps : [{
     name   : "care_why_api",
+    cwd: "/home/carewhyapp/apps_nodejs/care_why_api/",
     script : "./src/server.js",
     watch: true,
     ignore_watch: ["upload", "database"],
