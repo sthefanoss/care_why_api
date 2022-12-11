93123438ae0aa1434e8b9771444c40726400a983
sthefano <sthefanoss@gmail.com>
2022-12-11 17:06:52 -0300

    test

--- src/server.js
+++ src/server.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(fileStoragePath, express.static("upload"));
+app.use(express.static(fileStoragePath));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
