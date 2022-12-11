2f16ade11955378d5d9dcfcd981ddf49696e65db
sthefano <sthefanoss@gmail.com>
2022-12-11 17:28:28 -0300

    f

--- src/utils/file_storage.js
+++ src/utils/file_storage.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const dirname = require("../../dirname");
 
-const path = dirname + '/uploads';
+const path = dirname + '/uploads/';
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
